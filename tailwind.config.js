273cab02efce2ae4655c3e0927ecec7dac19d428
khatabwedaa <khatabwedaa@gmail.com>
2020-02-07 22:22:42 +0200

    create tailwind config & added google font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto'],
+        'tajawal': ['Tajawal'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
