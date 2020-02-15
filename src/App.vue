7a8de6a1fe57392be74f04b327e55808e462a37a
khatabwedaa <khatabwedaa@gmail.com>
2020-02-15 12:36:31 +0200

    edit scroll style

--- src/App.vue
+++ src/App.vue
@@ -103,4 +103,16 @@ export default {
 
 <style>
   @import "assets/css/app.css";
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-track{
+    background: #2D3748;
+  }
+  ::-webkit-scrollbar-thumb{
+    background: #CBD5E0;
+  }
+  ::-webkit-scrollbar-thumb:hover{
+    background: #2D3748;
+  }
 </style>
